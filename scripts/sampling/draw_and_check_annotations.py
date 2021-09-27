162cfa0184436861f235d5a05d1836eb3aab6c85
LiamTTT <liam.liusibo@gmail.com>
2021-09-27 11:23:51 +0800

    init commit.

---
+++ scripts/sampling/draw_and_check_annotations.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+'''
+@Author: LiamTTT
+@Email: nirvanalautt@gmail.com
+@Project: TCTPreprocess
+@File: draw_and_check_annotations.py.py
+@Date: 2021/9/22 
+@Time: 下午12:17
+@Desc:
+'''
+# todo 20210922 siboliu: draw and check annotations.
---
+++ scripts/sampling/sampling_PMScope.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+'''
+@Author: LiamTTT
+@Email: nirvanalautt@gmail.com
+@Project: TCTPreprocess
+@File: sampling_PMScope.py
+@Date: 2021/9/22 
+@Time: 下午12:12
+@Desc: Create jpeg image according to Annotation xml files of PMScope.
+'''
+import os
+import json
+import argparse
+from time import time
+from threading import Thread
+
+
+if __name__ == '__main__':
+    pass

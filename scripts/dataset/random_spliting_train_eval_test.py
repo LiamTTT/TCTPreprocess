82c0ff2a960bc8d23832a9f4f34f2753d5b71274
LiamTTT <liam.liusibo@gmail.com>
2021-10-14 14:28:07 +0800

    add splitting according to given splitting json file

--- scripts/dataset/random_spliting_train_eval_test.py
+++ scripts/dataset/random_spliting_train_eval_test.py
@@ -28,13 +28,22 @@ if __name__ == '__main__':
     parser.add_argument('--ratios', type=float, nargs='+', default=[8, 1, 1],
                         help='ratios for trains test val dataset.')
     parser.add_argument('--seed', type=int, default=42, help='random seed.')
+    parser.add_argument('--splitting_dict', type=str, help='pre-define splitting dict of slides name.', default=None)
+    parser.add_argument('--roi', help="whether contain roi info.", action='store_true')
     args = parser.parse_args()
     SET_NAME = ['train', 'eval', 'test']
     SEED = args.seed
+    splitting_dict = args.splitting_dict
+    flg_roi = args.roi
     dataset_root = args.dataset_root
     dataset_root = os.path.abspath(dataset_root)
     logger.info(f'Processing {dataset_root}')
     ratios = args.ratios
+    if splitting_dict is not None:
+        logger.warning(f'Using pre define splitting, the splitting ratio will not be guaranteed: {splitting_dict}')
+        splitting_dict = json.load(open(splitting_dict, 'r'))
+    if flg_roi:
+        logger.warning('Slide is consisted by rois.')
     if not isinstance(ratios, list) or len(ratios) == 0:
         logger.error('arg --ratios get a wrong format!')
         raise ValueError('arg --ratios get a wrong format!')
@@ -69,13 +78,35 @@ if __name__ == '__main__':
         raise FileNotFoundError('no annotations and samples.')
     # get total samples and slides
     annotations = os.listdir(annotation_dir)
-    slide_name = list(set([an.split('_000_')[0] for an in annotations if '_000_' in an]))
+    if flg_roi:
+        slide_name = list(set(['_'.join(an.split('_000_')[0].split('_')[:-1]) for an in annotations if '_000_' in an]))
+    else:
+        slide_name = list(set([an.split('_000_')[0] for an in annotations if '_000_' in an]))
     # compute numbers of each dataset
     nb_all_samples = len(annotations)
     ls_nb_set = nb_all_samples * ratios / ratios.sum()
     ls_nb_set = ls_nb_set.tolist()
     float_ratio = 0.05
     # splitting
+    # pre splitting
+    pre_ls_samples = [[], [], []]
+    pre_ls_slides = [[], [], []]
+    if splitting_dict is not None:
+        logger.warning('Do splitting according to pre defined splitting json file. '
+                       'Slides not mentioned in pre-defined file will be splitted according to ratios.')
+        pre_ls_slides = [splitting_dict[k] for k in SET_NAME]
+        for i, sld_ns in enumerate(pre_ls_slides):
+            for cur_sld_n in sld_ns:
+                if cur_sld_n not in slide_name:
+                    continue
+                pre_ls_samples[i] += [os.path.basename(f).split('.jpg')[0] for f in
+                                      glob(os.path.join(jpegimages_dir, f'{cur_sld_n}_*.jpg'))]
+                slide_name.remove(cur_sld_n)
+            logger.info(
+                'Pre-splitting {} set -- slide num: {} sample num: {}'.format(
+                    SET_NAME[i], len(pre_ls_slides[i]), len(pre_ls_samples[i])
+                ))
+    # random splitting
     random.seed(SEED)  # FIXME 20210923 siboliu: the results is still random.
     ls_samples = [[], [], []]
     ls_slides = [[], [], []]
@@ -88,7 +119,7 @@ if __name__ == '__main__':
                 logger.warning(f'can not get a good solution for this splitting {nb_set}')
                 break
             cur_sld_n = random.sample(slide_name, 1)[0]
-            cur_samples = [os.path.basename(f) for f in glob(os.path.join(jpegimages_dir, f'{cur_sld_n}_*.jpg'))]
+            cur_samples = [os.path.basename(f).split('.jpg')[0] for f in glob(os.path.join(jpegimages_dir, f'{cur_sld_n}_*.jpg'))]
             cur_samples_nb = len(cur_samples)
 
             if cur_nb_set + cur_samples_nb < nb_set*(1-float_ratio):
@@ -116,7 +147,9 @@ if __name__ == '__main__':
                 ls_samples[i] += cur_samples
                 cur_nb_set += cur_samples_nb
                 ls_slides[i].append(cur_sld_n)
-
+        # add pre splitting
+        ls_samples[i] += pre_ls_samples[i]
+        ls_slides[i] += pre_ls_slides[i]
         logger.info('{} set -- slide num: {} sample num: {}'.format(SET_NAME[i], len(ls_slides[i]), len(ls_samples[i])))
 
     open(train_file_path, 'w+').writelines(sorted([l+'\n' for l in ls_samples[0]]))
